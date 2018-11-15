1ead5e600dc5924c363ad117ab9a4fd39e3eb649
rebfarish <rebfarish@gmail.com>
2018-11-15 10:17:51 -0700

    Initial commit

---
+++ src/edu/cnm/deepdive/rps/model/Neighborhood.java
@@ -0,0 +1,14 @@
+package edu.cnm.deepdive.rps.model;
+
+import java.util.Random;
+
+public interface Neighborhood {
+Location[] neighbors();
+
+default Location randomNeighbor(Random rng){
+  Location[] neighbors = this.neighbors();
+  return neighbors[rng.nextInt(neighbors.length)];
+}
+
+
+}
