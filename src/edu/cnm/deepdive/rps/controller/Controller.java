0dc4a1b479d0b1318be4d502ce7e16de337a6b95
rebfarish <rebfarish@gmail.com>
2018-11-15 11:52:07 -0700

    Added controller class and resources.

---
+++ src/edu/cnm/deepdive/rps/controller/Controller.java
@@ -0,0 +1,46 @@
+package edu.cnm.deepdive.rps.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.Slider;
+import javafx.scene.text.Text;
+
+public class Controller {
+
+  @FXML
+  private CheckBox fitCheckbox;
+  @FXML
+  private Text interationsLabel;
+  @FXML
+  private ScrollPane viewScroller;
+  @FXML
+  private Slider speedSlider;
+  @FXML
+  private Slider mixingSlider;
+  @FXML
+  private Button start;
+  @FXML
+  private Button stop;
+  @FXML
+  private Button reset;
+
+  @FXML
+  private void fitView(ActionEvent actionEvent) {
+  }
+
+  @FXML
+  private void start(ActionEvent actionEvent) {
+  }
+
+  @FXML
+  public void stop(ActionEvent actionEvent) {
+
+  }
+
+  @FXML
+  private void reset(ActionEvent actionEvent) {
+  }
+}
